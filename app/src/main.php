ea2d3c01e204504276b30e6f664fe2f4be34c596
Aayush Rijal <arijal@aiims.com.au>
2023-02-06 16:03:13 +1100

    Initial commit

---
+++ app/src/main.php
@@ -0,0 +1,154 @@
+<header>
+    <div class="container">
+        <div class="row justify-content-center justify-content-lg-between align-items-center gy-4">
+            <div class="col-auto">
+                <a href="./">
+                    <?= renderImg("logo.png", "logo") ?>
+                </a>
+            </div>
+            <div class="col-auto">
+                <div class="font-articulat text-white">More Options. <span class="fw-900 font-articulat-heavy">More You</span><span class="text-secondary">.</span></div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="banner pt-4">
+    <div class="container">
+        <div class="row align-items-end">
+            <div class="col-md-6 py-4 text-center text-md-start">
+                <div class="fs-33 fw-900 lh-1">Meet Julian & Robbie<span class="text-secondary">.</span></div>
+                <div class="pt-2 pb-4 fs-11">Come find us at the Better Business Summit</div>
+                <div class=""><?= renderImg('summit-awards.png', 'lib') ?></div>
+            </div>
+            <div class="col-md-auto">
+                <?= renderImg('julian-and-robbie.png', 'lib', 'w-100') ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="partner">
+    <div class="container">
+        <div class="row justify-content-center align-items-center gy-5">
+            <div class="col">
+                <div class="text-primary fw-900 fs-36 lh-1 text-center text-md-start">Become a LoanOptions Partner<span class="text-secondary">.</span></div>
+            </div>
+            <div class="col-auto"><?= renderImg('superhero.png', 'lib') ?></div>
+        </div>
+    </div>
+</section>
+
+<section class="affiliate-partner">
+    <div class="container">
+        <div class="affiliate-partner-img">
+            <?= renderImg('affiliate-partner.jpg', 'lib') ?>
+            <button class="play-button"><?= renderImg('play-button.png', 'lib') ?></button>
+        </div>
+    </div>
+</section>
+
+<section class="seamless-integration">
+    <div class="container">
+        <div class="row justify-content-center align-items-center gy-4">
+            <div class="col-auto"><?= renderImg('seamless-integration.png', 'lib') ?></div>
+            <div class="col-md">
+                <div class="fw-900 fs-20 pb-2 lh-1">Seamless Integration<span class="text-secondary">.</span></div>
+                <div class="fs-12">
+                    <p class="pb-2">LoanOptions has launched our white labeled application journey. We can deploy this loan application journey on any website (even if your current one is old) and in any colours that suit your brand.</p>
+                    <div>This seamless low code integration uses AI to match clients with more than 68 lenders to get them the best deal. Specialising in car loans, personal loans, business loans and equipment finance.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="implementation">
+    <div class="container">
+        <div class="row justify-content-center align-items-center gy-4">
+            <div class="col-md order-2 order-md-1">
+                <div class="text-quartinary fw-900 fs-17 pb-2">It Can Be Yours Today<span class="text-secondary">.</span></div>
+                <div class="fs-12 fw-500">
+                    <p class="pb-2">Implementation is seamless and it takes <span class="fw-700">less than a day</span>. Your company can offer the same transparent quoting tools and matching technology embedded natively on your website!</p>
+                    <div>We got so sick of seeing boring “enquire now” finance enquiry forms that lead nowhere and fail to offer the customer any real value from our dinosaur industry.</div>
+                </div>
+            </div>
+            <div class="col-auto order-1 order-md-2"><?= renderImg('implementation.png', 'lib') ?></div>
+        </div>
+    </div>
+</section>
+
+<section class="solution">
+    <div class="container">
+        <div class="fs-20 fw-900 pb-4">With This Solution You Can<span class="text-secondary">...</span></div>
+        <div class="row gx-2 gy-4">
+            <div class="col-6 col-md-3">
+                <?= renderImg('solution-1.png', 'lib', 'w-100') ?>
+                <div class="pt-3 px-2 text-center fs-9 lh-1 fw-900">Capture Enquiries with Verified Phone Numbers</div>
+            </div>
+            <div class="col-6 col-md-3">
+                <?= renderImg('solution-2.png', 'lib', 'w-100') ?>
+                <div class="pt-3 px-2 text-center fs-9 lh-1 fw-900">Capture Full<br> Applications</div>
+            </div>
+            <div class="col-6 col-md-3">
+                <?= renderImg('solution-3.png', 'lib', 'w-100') ?>
+                <div class="pt-3 px-2 text-center fs-9 lh-1 fw-900">Capture Supporting Documents</div>
+            </div>
+            <div class="col-6 col-md-3">
+                <?= renderImg('solution-4.png', 'lib', 'w-100') ?>
+                <div class="pt-3 px-2 text-center fs-9 lh-1 fw-900">Have All Compliance Documents Digitally Signed</div>
+            </div>
+        </div>
+        <div class="fs-20 fw-900 pt-4 text-end">Completely Seamless<span class="text-secondary">...</span></div>
+    </div>
+</section>
+
+<section class="for-whom">
+    <div class="container">
+        <div class="row justify-content-center align-items-center gx-md-5 gy-4">
+            <div class="col-md order-2 order-md-1">
+                <div class="fs-20 fw-900 pb-3">Who is this for?</div>
+                <div class="fs-12">
+                    <p class="">The customer’s benefit from the technology, but this is aimed at helping Brokers, lead generators, equipment vendors, medical practices, dealerships, accountants, financial planners, travel agencies and anyone else who needs to offer their clients competitive and transparent loan options.</p>
+                    <div>Due to the size & scale of our lending panel, it also provides other asset finance brokers additional lenders with whom they may not hold an accreditation with. It allows them to capture a lead or an entire application from start to finish including supporting documentation and digitally signed documents.</div>
+                    <a href="" class="btn d-inline-flex btn-secondary rounded-pill text-white fw-700 px-4 mt-4">Become A Partner Today</a>
+                </div>
+            </div>
+            <div class="col-auto order-1 order-md-2"><?= renderImg('sb.png', 'lib') ?></div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+        <div class=""><?= renderImg('logo.png', 'logo') ?></div>
+        <div class="text-light fs-11 py-4">
+            <div>Building F, Level 4, Suite 2/1 Homebush Bay Drive,</div>
+            <div>Rhodes NSW 2138</div>
+        </div>
+        <div class="fs-11">
+            <div class="fs-600 text-white">More options. More you.</div>
+            <div class="text-primary-light">Compare realistic loan options with powerful data & AI mathcing<br> technology.</div>
+        </div>
+        <div class="row justify-content-center gx-2 py-4">
+            <div class="col-auto">
+                <a href="" class=""><?= renderImg("fb.png", 'lib') ?></a>
+            </div>
+            <div class="col-auto">
+                <a href="" class=""><?= renderImg("twitter.png", 'lib') ?></a>
+            </div>
+            <div class="col-auto">
+                <a href="" class=""><?= renderImg("instagram.png", 'lib') ?></a>
+            </div>
+            <div class="col-auto">
+                <a href="" class=""><?= renderImg("youtube.png", 'lib') ?></a>
+            </div>
+        </div>
+        <div><?= renderImg('algorithm.png', 'lib') ?></div>
+        <div class="row gx-2 justify-content-center pt-4">
+            <div class="col-auto text-center"><a href="" class="fs-11 text-primary-light text-decoration-none"><u>Discover Loan Options</u></a></div>
+            <div class="col-auto text-center"><a href="" class="fs-11 text-primary-light text-decoration-none"><u>Contact Us</u></a></div>
+            <div class="col-auto text-center"><a href="" class="fs-11 text-primary-light text-decoration-none"><u>Unsubscribe</u></a></div>
+        </div>
+    </div>
+</footer>
